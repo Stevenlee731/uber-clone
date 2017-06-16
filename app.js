b6fcf2a385a6b9cda69bd66ff6975559ef13505e
stevecreated@gmail.com <stevecreated@gmail.com>
2017-06-16 14:09:18 -0700

    create driver

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const routes = require('./routes/routes')
+const mongoose = require('mongoose')
 const app = express() //takes in incoming http requests
 const bodyParser = require('body-parser')
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/uber-clone')
 
 app.use(bodyParser.json())
 routes(app)
--- models/driver.js
+++ models/driver.js
@@ -5,7 +5,7 @@ const DriverSchema = new Schema({
   email: {
     type: String,
     required: true
-  }
+  },
   driving: {
     type: Boolean,
     default: false
