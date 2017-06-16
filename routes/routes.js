0863590b7308b2c2031e9cc80540db7e5cef4388
stevecreated@gmail.com <stevecreated@gmail.com>
2017-06-16 16:12:31 -0700

    testing for editing driver

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,7 @@ module.exports = (app) => {
   //Watch for incoming requiest fo method POST
   // to the route http://localhost:3050/api/drivers
   app.post('/api/drivers', DriversController.create)
+  // :id is wildcard that express will parse in the request
+  app.put('/api/drivers/:id', DriversController.edit)
+  // app.delete('/api/drivers/:id', DriversController.delete)
 }
